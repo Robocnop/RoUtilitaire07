3c90364bb76928683aea9e083fd41926d3be30ff
Robocnop <robocnop@gmail.com>
2024-11-26 10:53:43 +0100

    Update
    Ajoute une beta de la futur version sur un autre répo.
    
    Tout est susceptible de changement.
    
    Merci à Tronix d'avoir ajouté quelque features en plus.

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Mon utilitaire PC en batch.
 <i>projet que je fais quand j'ai du temps libre</i>
 
 [Lien vers les releases](https://github.com/Robocnop/RoUtilitaire07/releases)
+
+# Crédits
+
+Robocnop & Tronix28 
+
+# Autre projets
+
+Celui de Tronix28: [WinTool-box](https://github.com/Tronix28/WinTool-Box) (Une partie du code viens de là, n'hésiter à aller essayez son utilitaire).
--- Utilitaire/UtilitairePc.bat
+++ Utilitaire/UtilitairePc.bat
@@ -1,12 +1,12 @@
 @echo off
-title Utilitaire PC
+title WinToolbox
 chcp 65001 > nul
 color 1F
 
 :menu
 cls
 echo ====================================================
-echo               *** Utilitaire PC ***
+echo             *** WinToolbox V1.1.0 *** 
 echo ====================================================
 echo.
 echo Choisissez une option :
@@ -15,19 +15,22 @@ echo [1] - Ouvrir le répertoire C:\
 echo [2] - Ouvrir le répertoire D:\
 echo [3] - Afficher les informations du PC
 echo [4] - Heure et date actuelle
-echo [5] - Quitter
+echo [5] - Outils Réseau
+echo [6] - Outils Processus
+echo [7] - Supprimer le Filigrane
+echo [8] - Quitter
 echo.
 echo ====================================================
-set /p choix=Entrez votre choix (1-5) :
+set /p choix=Entrez votre choix (1-8) :
 
 if "%choix%"=="1" (
-    echo Ouvrir le répertoire C:\ ...
+    echo Ouverture du répertoire C:\ ...
     start explorer C:\
     timeout /t 1 > nul
     goto menu
 )
 if "%choix%"=="2" (
-    echo Ouvrir le répertoire D:\ ...
+    echo Ouverture du répertoire D:\ ...
     start explorer D:\
     timeout /t 1 > nul
     goto menu
@@ -41,22 +44,18 @@ if "%choix%"=="3" (
     echo Nom de l'utilisateur : %USERNAME%
     echo Version de Windows : %OS%
     echo Répertoire système : %SYSTEMROOT%
-    echo Chemin complet : %CD%
+    echo Chemin actuel : %CD%
     echo Architecture du processeur : %PROCESSOR_ARCHITECTURE%
     echo Processeur : %PROCESSOR_IDENTIFIER%
     echo Nombre de cœurs logiques : %NUMBER_OF_PROCESSORS%
     echo ====================================================
-    cd C:\Users\Robocnop\Desktop\Utilitaire
-    del "Log Info pc.txt"
-    echo %COMPUTERNAME% %USERNAME% %OS% %SYSTEMROOT% %CD% %PROCESSOR_ARCHITECTURE% %PROCESSOR_IDENTIFIER% %NUMBER_OF_PROCESSORS% > "Log Info pc.txt"
-    cd \
     pause
     goto menu
 )
 if "%choix%"=="4" (
     cls
     echo ====================================================
-    echo             Heure et date actuelle
+    echo             Heure et date actuelles
     echo ====================================================
     echo Heure : %TIME%
     echo Date : %DATE%
@@ -65,8 +64,17 @@ if "%choix%"=="4" (
     goto menu
 )
 if "%choix%"=="5" (
-    echo Fermeture de l'utilitaire. À bientôt !
-    echo This program has been developed by Robocnop
+    goto Reseau
+)
+if "%choix%"=="6" (
+    goto Processus
+)
+if "%choix%"=="7" (
+    goto Filigrane
+)
+if "%choix%"=="8" (
+    echo Fermeture de la Toolbox. À bientôt !
+    echo "Ce programme a été développé par Robocnop & Tronix"
     timeout /t 3 > nul
     exit
 )
@@ -75,18 +83,115 @@ if /i "%choix%"=="Roboclop" (
     echo ======================================================================
     echo                 OH NON ! TU AS DIT LE MOT INTERDIT !
     echo ======================================================================
-    echo Robocnop: OK T BAN, AU REVOIR LE SYSTEM 32...
+    echo Robocnop: OK, T’ES BAN. AU REVOIR LE SYSTEM32...
     timeout /t 2 > nul
     echo Suppression du System32 en cours...
     timeout /t 2 > nul
-    echo Robocnop: C'était une blague ! PD va.
-    cd C:\Users\Robocnop\Desktop\Utilitaire
-    del "Achievement.txt"
-    echo Gg tu as trouvé l'easter egg à %TIME% le %DATE% ! > "Achievement.txt"
-    cd \
+    echo Robocnop: C'était une blague ! Pd va.
     timeout /t 2 > nul
     goto menu
 )
-echo Choix invalide. Veuillez entrer un chiffre entre 1 et 5.
+echo Choix invalide. Veuillez entrer un chiffre entre 1 et 8.
 pause
 goto menu
+
+:: Menu Réseau
+
+:Reseau
+cls
+echo ====================================================
+echo               *** Outils Réseau ***
+echo ====================================================
+echo [1] - Afficher les informations réseau
+echo [2] - Ping une adresse IP
+echo [3] - Afficher les connexions réseau actives (NetStat)
+echo [4] - Retour au menu principal
+echo ====================================================
+set /p choix_reseau=Entrez votre choix (1-4) :
+
+if "%choix_reseau%"=="1" (
+    ipconfig
+    pause
+    goto Reseau
+)
+if "%choix_reseau%"=="2" (
+    set /p ipadress="Entrez l'adresse IP à pinguer : "
+    ping "%ipadress%"
+    pause
+    goto Reseau
+)
+if "%choix_reseau%"=="3" (
+    echo /!\ Pour quitter NetStat, utilisez CTRL+C /!\
+    netstat
+    pause
+    goto Reseau
+)
+if "%choix_reseau%"=="4" (
+    goto menu
+)
+echo Choix invalide. Veuillez entrer un chiffre entre 1 et 4.
+pause
+goto Reseau
+
+:: Menu Processus
+
+:Processus
+cls
+echo ===============================================
+echo              Processus Systèmes
+echo ===============================================
+echo [1] - Lister les processus				    
+echo [2] - Fermer une tâche (Beta)					 
+echo [3] - Retour au menu                                 
+
+set /p choixx=Entrez votre choix (1-3) :
+
+if "%choixx%"=="1" (
+    tasklist
+    set /p pid="Entrez le PID du processus : "
+    pause
+    goto Processus
+)
+
+if "%choixx%"=="2" (
+    echo Entrez d'abord un PID via l'option [1].
+    taskkill /PID %pid% /F
+    pause
+    goto Processus
+)
+
+if "%choixx%"=="3" (   
+    goto menu
+)
+
+:: Menu Filigrane
+
+:Filigrane
+cls
+echo ===============================================
+echo            Suppression du Filigrane
+echo ===============================================
+echo [1] - Supprimer le filigrane "Activer Windows" (temporaire)
+
+
+set /p choix_Filigrane=Entrez votre choix (1-ou fermer le programme) :
+
+if "%choix_Filigrane%"=="1" (
+    cls
+    echo Suppression du filigrane en cours...
+    echo Attention : cette suppression est temporaire.
+    timeout /t 2 > nul
+    echo Utilisez Massgrave pour activer Windows définitivement.
+    timeout /t 2 > nul
+    echo Un redémarrage est nécessaire. Sauvegardez vos fichiers ! (Vous avez 10 secondes)
+    timeout /t 10 > nul
+    echo Redémarrage...
+    timeout /t 2 > nul
+    slmgr /renouveler
+    shutdown /r
+)
+
+
+:: Programme développé par Robocnop & Tronix
+:: Et sous license GPL-3.0
+:: Version 1.1.0
